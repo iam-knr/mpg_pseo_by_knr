fbffbdcec43f644e19e56a6baeeb909d2b09eb54
iam-knr <nathrkailas@gmail.com>
2026-02-22 13:45:36 +0530

    Bump version to 2.0.1

--- pseo-bulk-generator.php
+++ pseo-bulk-generator.php
@@ -3,7 +3,7 @@
  * Plugin Name: PSEO PRO by KNR
  * Plugin URI:  https://linkedin.com/in/iamknr
  * Description: Generate thousands of SEO-optimised pages from CSV, Google Sheets, JSON or REST API. Unlimited rows, built-in schema, meta, sitemap, cron & WP-CLI.
- * Version:     1.0.0
+ * Version:     2.0.1
  * Author:      Kailas (KNR) Nath R
  * Author URI:  https://linkedin.com/in/iamknr
  * License:     GPL-2.0-or-later
